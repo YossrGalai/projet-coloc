7bc373e401e0d07e330f0513e3de65daa79568ab
Yossr Galai <yossrgalai02@gmail.com>
2025-12-03 15:42:06 +0100

    Fix connection.js

--- backend/backend-colocation/db/connection.js
+++ backend/backend-colocation/db/connection.js
@@ -3,9 +3,12 @@ const oracledb = require('oracledb');
  async function getConnection() {
   try {
     return await oracledb.getConnection({
-      user: "C##emnaa",
-      password: "000000", 
-      connectString: "localhost:1521/XE" 
+      //user: "C##emnaa",
+      //password: "000000", 
+      //connectString: "localhost:1521/XE"
+      user: "system",
+      password: "yossr2004", 
+      connectString: "localhost:1521/XE"
     });
   } catch (err) {
     console.error("Erreur de connexion à Oracle :", err);
